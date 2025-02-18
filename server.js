092453757890dd836c12e6fbacf5cf6b26e99ab2
liammelkersson <liammelkersson@live.se>
2025-02-18 14:37:51 +0100

    css shi
    some intial css just for fun

--- server.js
+++ server.js
@@ -6,6 +6,7 @@ const fs = require('fs');
 const app = express();
 const PORT = 3000;
 
+// define static directory "public"
 app.use("/", express.static("public"));
 
 // ========== DEFINING ROUTES ==========
